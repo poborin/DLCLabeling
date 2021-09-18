3217d2a5e97feaff873506a600db5560b59b315a
Pavel Oborin <pavel.oborin@gmail.com>
2021-09-18 22:18:56 +1000

    menu

--- src/Labeling.fs
+++ src/Labeling.fs
@@ -8,85 +8,141 @@ open ConfigDecoder
 open System
 
 type State = { 
-    Config: MinimalConfig }
+    Config: MinimalConfig 
+    ShowQuickView: bool }
 
 type Props = { Config: MinimalConfig }
 
 type Msg = 
     | DisaplayConfig of MinimalConfig
     | DisaplayConfigFailed
+    | ToggleQuickView
 
-let init props = { Config = props.Config }, Cmd.none
+let init props = { Config = props.Config; ShowQuickView = false }, Cmd.none
 
 let update props msg state =
     match msg with 
     | DisaplayConfig config -> state, Cmd.none
     | DisaplayConfigFailed -> state, Cmd.none
+    | ToggleQuickView -> { state with ShowQuickView = not state.ShowQuickView }, Cmd.none
  
 let labelingCanvas = React.functionComponent("LoadConfigForm", fun props -> 
     let state, dispatch = React.useElmish(init props, update props, [| |])
-
-    Bulma.columns [
-        Bulma.column [
+    Html.div [
+        Bulma.navbar [
+            Bulma.color.isPrimary
             prop.children [
-                Bulma.section [
-                    Bulma.image [
-                        Bulma.image.isFullWidth
-                        prop.children [
-                            Html.image [
-                                prop.src "https://bulma.io/images/placeholders/1280x960.png"
+                Bulma.navbarBrand.div [
+                    Bulma.navbarItem.a [
+                        Bulma.icon [ Html.i [ prop.className "fas fa-draw-polygon fa-2x" ] ]
+                    ]
+                ]
+                Bulma.navbarMenu [
+                    Bulma.navbarStart.div [
+                        Bulma.navbarItem.div [
+                            Bulma.navbarItem.hasDropdown
+                            Bulma.navbarItem.isHoverable
+                            prop.children [
+                                Bulma.navbarLink.a [ prop.text "File" ]
+                                Bulma.navbarDropdown.div [
+                                    Bulma.navbarItem.a [ prop.text "Load images" ]
+                                    Bulma.navbarDivider []
+                                    Bulma.navbarItem.a [ prop.text "Save" ]
+                                ]
+                            ]
+                        ]
+                        Bulma.navbarItem.a [ prop.text "Documentation" ]
+                    ]
+                    Bulma.navbarEnd.div [
+                        Bulma.navbarItem.div [
+                            Bulma.buttons [
+                                Bulma.button.a [
+                                    Bulma.icon [ 
+                                        prop.onClick (fun _ -> ToggleQuickView |> dispatch )
+                                        prop.children [
+                                            Html.i [ prop.className "fas fa-cogs" ] 
+                                        ]
+                                    ]
+                                ]
                             ]
                         ]
                     ]
                 ]
             ]
         ]
-        Bulma.column [
-            Bulma.column.is2
-            prop.children [
-                Divider.divider [
-                    prop.text "Labeling"
+
+        QuickView.quickview [
+            if state.ShowQuickView then yield quickview.isActive
+            yield prop.children [
+                QuickView.header [
+                    Html.div "Config"
+                    Bulma.delete [ prop.onClick (fun _ -> ToggleQuickView |> dispatch) ]
                 ]
-                Html.p [
-                    Html.strong "Individuals"
+                QuickView.body [
+                    QuickView.block "TODO: display config parameters"
                 ]
-                Bulma.control.p [
-                    prop.children [
-                        Bulma.select [
-                            state.Config.Individuals |> Array.map (fun x -> Html.option x) |> prop.children
-                        ]
-                    ]
-                ]
-                Html.p [
-                    Html.strong "Body parts"
-                ]
-                Html.div [
-                    state.Config.Multianimalbodyparts 
-                    |> Array.map (fun x -> 
-                        Bulma.field.div [ 
-                            Checkradio.radio [ prop.id x; prop.name "radio" ]
-                            Html.label [ prop.htmlFor x; prop.text x ]
-                        ])
-                    |> prop.children
-                ]
-                Divider.divider [
-                    prop.text "Images"
-                ]
-                Bulma.label [
-                    prop.children [
-                        Bulma.fileInput [
-                            
-                        ]
-                        Html.span [
-                            prop.className "file-cta"
+            ]
+        ]
+
+        Bulma.columns [
+            Bulma.column [
+                prop.children [
+                    Bulma.section [
+                        Bulma.image [
+                            Bulma.image.isFullWidth
                             prop.children [
-                                Html.span [
-                                    prop.className "file-label"
-                                    prop.text "Load images"
+                                Html.image [
+                                    prop.src "https://bulma.io/images/placeholders/1280x960.png"
                                 ]
                             ]
                         ]
                     ]
+                ]
+            ]
+            Bulma.column [
+                Bulma.column.is2
+                prop.children [
+                    Html.p [
+                        Html.strong "Individuals"
+                    ]
+                    Bulma.control.p [
+                        prop.children [
+                            Bulma.select [
+                                state.Config.Individuals |> Array.map (fun x -> Html.option x) |> prop.children
+                            ]
+                        ]
+                    ]
+                    Html.p [
+                        Html.strong "Body parts"
+                    ]
+                    Html.div [
+                        state.Config.Multianimalbodyparts 
+                        |> Array.map (fun x -> 
+                            Bulma.field.div [ 
+                                Checkradio.radio [ prop.id x; prop.name "radio" ]
+                                Html.label [ prop.htmlFor x; prop.text x ]
+                            ])
+                        |> prop.children
+                    ]
+                    // Divider.divider [
+                    //     prop.text "Images"
+                    // ]
+                    // Bulma.label [
+                    //     prop.children [
+                    //         Bulma.fileInput [
+                                
+                    //         ]
+                    //         Html.span [
+                    //             prop.className "file-cta"
+                    //             prop.children [
+                    //                 Html.span [
+                    //                     prop.className "file-label"
+                    //                     prop.text "Load images"
+                    //                 ]
+                    //             ]
+                    //         ]
+                    //     ]
+                    // ]
                 ]
             ]
         ]
